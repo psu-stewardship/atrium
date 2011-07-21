54015ec5e981a2a4af7ec9d77616f93ddfbf4fe5
rickjohnson <rick.johnson@nd.edu>
2011-07-21 14:55:05 -0400

    Initial checkin as Blacklight Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atrium::Application.config.secret_token = '71872e790ba46252f035e94193f5196c5c8c62b6a80c3a1e46e4cc4cc3f1559815d6b6c486720207fb233e49cd0b94bd17febc5eb3d1b819a65703670abb5228'
