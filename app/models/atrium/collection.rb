eeda64534cebfef9e44014e5d55cc5779a824afe
Dan Brubaker Horst <dbrubak1@nd.edu>
2011-12-02 14:58:29 -0500

    Markup cleanup

--- app/models/atrium/collection.rb
+++ app/models/atrium/collection.rb
@@ -1,9 +1,9 @@
 class Atrium::Collection < ActiveRecord::Base
-  has_many :exhibits,     :class_name=>'Atrium::Exhibit',      :foreign_key=>'atrium_collection_id', :order=>'set_number ASC', :dependent => :destroy
-  has_many :search_facets, :class_name=>'Atrium::Search::Facet', :foreign_key=>'atrium_collection_id', :dependent => :destroy
-  has_many :showcases,  :class_name => 'Atrium::Showcase',  :as=>:browse_details
+  has_many :exhibits,      :class_name => 'Atrium::Exhibit',       :foreign_key => 'atrium_collection_id', :order => 'set_number ASC', :dependent => :destroy
+  has_many :search_facets, :class_name => 'Atrium::Search::Facet', :foreign_key => 'atrium_collection_id', :dependent => :destroy
+  has_many :showcases,     :class_name => 'Atrium::Showcase',      :as => :browse_details
 
-  accepts_nested_attributes_for :exhibits,     :allow_destroy => true
+  accepts_nested_attributes_for :exhibits,      :allow_destroy => true
   accepts_nested_attributes_for :search_facets, :allow_destroy => true
 
   set_table_name :atrium_collections
--- app/models/atrium/description.rb
+++ app/models/atrium/description.rb
@@ -1,7 +1,7 @@
 class Atrium::Description < ActiveRecord::Base
-  belongs_to :showcase, :class_name=>'Atrium::Showcase', :foreign_key=>"atrium_showcase_id"
+  belongs_to :showcase, :class_name => 'Atrium::Showcase', :foreign_key => "atrium_showcase_id"
 
-  validates_presence_of  :atrium_showcase_id
+  validates_presence_of :atrium_showcase_id
 
   set_table_name :atrium_descriptions
 
